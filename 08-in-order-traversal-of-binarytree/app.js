fcc39ffaf152e521299996b152c610816cbef90c
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-06-01 12:03:21 +0530

    module changed using es6

--- 08-in-order-traversal-of-binarytree/app.js
+++ 08-in-order-traversal-of-binarytree/app.js
@@ -1,4 +1,4 @@
-const BinaryTree = require("./service/in-order-traversal-service")
+import  BinaryTree from './service/in-order-traversal-service.js'
 const binaryTree = new BinaryTree();
 let data =[3,4,5,6,7,9,10,17,20,22]
 for(let index=0; index<data.length;index++){
