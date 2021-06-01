9e2cebc5bfd23d9ce0accf02e4b811e308821d0f
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-06-01 11:58:25 +0530

    module changed using es6

--- 07-preorder-traversal-of-binarytree/app.js
+++ 07-preorder-traversal-of-binarytree/app.js
@@ -1,4 +1,4 @@
-const BinaryTree = require("./service/pre-order-traversal-service")
+import BinaryTree from './service/pre-order-traversal-service.js';
 const binaryTree = new BinaryTree();
 let data =[9,4,17,3,6,22,5,7,20];
 for (let index=0;index<data.length;index++){
