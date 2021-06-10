81e866811381253eb6b83ee0d91d472ed46a42e5
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-06-10 09:26:24 +0530

    updated

--- 11-algorithm-async-promise-callback/algorithm-frontend/algorithm-frontend/src/app/service.ts
+++ 11-algorithm-async-promise-callback/algorithm-frontend/algorithm-frontend/src/app/service.ts
@@ -8,18 +8,10 @@ import { Observable } from 'rxjs/internal/Observable';
 export class Service {
 baseURL = "http://localhost:3000/api/";
     constructor(private http: HttpClient) { }
-    private API = "http://localhost:3000/findLargest"
+
  findAnagram(firstWord: string, secondWord: string) {
     
-    let result=  this.http.get(`${this.baseURL}anagram?firstWord=${firstWord}&secondWord=${secondWord}`).toPromise();
-    console.log(result) 
-    return result 
-      }
-
-   findletterOccurance (word:string){
-    let res =  this.http.get((`${this.baseURL}letterOccurance?word=${word}`)).toPromise();  
-    return res;
- }
+   
 
  findLargeNumber(array:number[],largestNumber:number){
     let res= this.http.post((`${this.baseURL}findlargest`),{array,largestNumber}).toPromise()
